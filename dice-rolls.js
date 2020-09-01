7d24bba43583ca64f940a5a0ebcbc195896cc167
Vance <coletain@gmail.com>
2020-09-01 11:04:30 -0700

    adds async explanation to rolls examples

--- dice-rolls.js
+++ dice-rolls.js
@@ -4,6 +4,17 @@ let r = new Roll('1d20').roll();
 // Roll to chat message
 new Roll('1d20').toMessage();
 
+/*
+ * Note that toMessage is async
+ * This means that if you need to use the result of the roll, you should either
+ * 1) do so before calling toMessage()
+ * 2) use await
+ */
+let r = new Roll('1d20').roll();
+r.toMessage();
+// or
+let r = await new Roll('1d20').toMessage();
+
 // Roll with flavor text
 new Roll('1d20').toMessage({ flavor: 'Example Flavor' });
 
