5bfdbae4f4935d33c4a697736965eddd1990ad88
Vance <coletain@gmail.com>
2020-09-02 00:25:13 -0700

    Adds complex flag example, animated-gif-to-webm, bulk webp convert

--- flags.js
+++ flags.js
@@ -7,3 +7,12 @@ let x = canvas.scene.getFlag('myModule', 'myFlag');
 // Unset a module flag on a scene
 await canvas.scene.unsetFlag('myModule', 'myFlag');
 
+// Flags can be arrays, objects, numbers etc as well
+// Anything that can be stringified
+let data = {
+  myString: 'hello',
+  myNum: 42,
+  myBool: true,
+  myArray: ['a','b','c']
+}
+await canvas.scene.setFlag('myModule', 'myFlag', data);
