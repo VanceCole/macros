eeb707b3f1ed6bf6f01c7d6ab5c42a67c41d5a41
Vance <coletain@gmail.com>
2020-09-06 10:06:27 -0700

    formapplication example

---
+++ form-application.js
@@ -0,0 +1,49 @@
+class MyFormApplication extends FormApplication {
+  constructor(exampleOption) {
+    super();
+    this.exampleOption = exampleOption;
+  }
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ['form'],
+      popOut: true,
+      template: `myFormApplication.html`,
+      id: 'my-form-application',
+      title: 'My FormApplication',
+    });
+  }
+
+  getData() {
+    // Return data to the template
+    return {
+      msg: this.exampleOption,
+      color: 'red',
+    };
+  }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+  }
+
+  async _updateObject(event, formData) {
+    console.log(formData.exampleInput);
+  }
+}
+
+window.MyPopup = MyPopup;
+
+/**
+ * myFormApplication.html
+ */
+<form class="flexcol">
+  <div class="form-group">
+    <label for="exampleInput">Example Input</label>
+    <input type="text" name="exampleInput" style="color: {{color}}" value="{{msg}}">
+  </div>
+  <footer class="sheet-footer flexrow">
+    <button type="submit" name="submit">
+        <i class="fa fa-check"></i> OK
+    </button>
+  </footer>
+</form>
