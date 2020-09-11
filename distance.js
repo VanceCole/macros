40ff6c3cdce16e1ad6a1fc5952c10891d0e65dd2
Vance <coletain@gmail.com>
2020-09-10 19:33:52 -0700

    Adds distance measurement macros

---
+++ distance.js
@@ -0,0 +1,24 @@
+/**
+ * Get euclidean distance between two placeable objects
+ */
+let a = canvas.tokens.placeables[0];
+let b = canvas.tokens.placeables[1];
+let dist = canvas.grid.measureDistance(a, b);
+
+/**
+ * Gets distance between two placeable objects
+ * for example, tokens, using 5e grid rules
+ * @param {Placeable} one 
+ * @param {Placeable} two 
+ */
+function get5eGridDist(one, two) {
+  let gs = canvas.grid.size;
+  let d1 = Math.abs((one.x - two.x) / gs);
+  let d2 = Math.abs((one.y - two.y) / gs);
+  let dist = Math.max(d1, d2);
+  dist = dist * canvas.scene.data.gridDistance;
+  return dist;
+}
+let a = canvas.tokens.placeables[0];
+let b = canvas.tokens.placeables[1];
+let dist = get5eGridDist(a, b);
