609470b71f7a0add87a3f7cf3b1da0a44d8d3246
Vance Cole <coletain@gmail.com>
2020-10-04 01:23:19 -0700

    Update bulk-convert-to-webp.ps1
    add echo

--- imagemagick/bulk-convert-to-webp.ps1
+++ imagemagick/bulk-convert-to-webp.ps1
@@ -3,4 +3,5 @@
 Get-ChildItem .\* -R -include *.png, *.jpg, *.jpeg, *.gif |
 Foreach-Object {
   magick convert -strip $_.FullName -quality 60 "$($_.FullName.split('.')[0]).webp"
+  echo "Converting $_"
 }
