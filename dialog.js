5d5508bb8dbfdd5586dc531dd87c1285f19c1083
Vance <coletain@gmail.com>
2020-10-10 02:00:58 -0700

    Adds more input types to example input dialog

--- dialog.js
+++ dialog.js
@@ -36,6 +36,22 @@ let d = new Dialog({
         <label for="exampleInput">Example Input</label>
         <input type="text" name="exampleInput" placeholder="Enter Value">
       </div>
+      <div class="form-group">
+        <label for="exampleSelect">Example Select</label>
+        <select name="exampleSelect">
+          <option value="option1">Option One</option>
+          <option value="option2">Option Two</option>
+          <option value="option3">Option Three</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="exampleColor">Example Color</label>
+        <input class="color" type="text" name="exampleColor" value="#ff6400">
+        <input type="color" value="#ff6400" data-edit="exampleColor">
+      </div>
+      <div class="form-group">
+        <textarea name="exampleText" placeholder="Enter Text"></textarea>
+      </div>
     </form>
   `,
   buttons: {
@@ -47,8 +63,11 @@ let d = new Dialog({
       icon: '<i class="fas fa-check"></i>',
       label: 'Yes',
       callback: (html) => {
-        let val = html.find('input[name="exampleInput"]').val();
-        console.log(`ExampleInput value was: ${val}`);
+        let input = html.find('[name="exampleInput"]').val();
+        let select = html.find('[name="exampleSelect"]').val();
+        let color = html.find('[name="exampleColor"]').val();
+        let text = html.find('[name="exampleText"]').val();
+        console.log(input, select, color, text);
       }
     },
   },
@@ -56,7 +75,7 @@ let d = new Dialog({
   close: () => {
     console.log('Example Dialog Closed');
   }
-}).render(true);
+}).render(true)
 
 /* ---------------------------------------------------- */
 
